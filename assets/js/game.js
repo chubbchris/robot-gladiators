d54dfe4096ccd15625c553ff108937f4696e6421
chris chubb <chubb@laptop-HRP37O3Q>
2022-02-21 16:22:06 -0500

    added player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("this is an alert! JavaScript is running")
+function fight() {
+    window.alert("the fight has begun!");
+    var playerName = window.prompt("what is your robot's name?");
+    // note the lack of quatation marks around playerName
+    console.log(playerName) ;
+    console.log("This logs a string,good for leaving yourself a message")
+    // This will do math and log 20
+    console.log(10+10);
+    //What is this?
+    console.log("our robot's name is "+ playerName);
+}
+
+fight();
