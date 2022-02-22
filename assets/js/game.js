0dc1219f9ca1d9e7cd407aace0dc4554b1f05cd5
chris chubb <chubb@laptop-HRP37O3Q>
2022-02-21 20:25:21 -0500

    added player interaction

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,26 @@
-function fight() {
-    window.alert("the fight has begun!");
-    var playerName = window.prompt("what is your robot's name?");
-    // note the lack of quatation marks around playerName
-    console.log(playerName) ;
-    console.log("This logs a string,good for leaving yourself a message")
-    // This will do math and log 20
-    console.log(10+10);
-    //What is this?
-    console.log("our robot's name is "+ playerName);
-}
+
+    var playerName = window.prompt("What is your robot's name?");
+    var playerHealth = 100;
+    var playerAttack = 10;
+
+    // you can also log multiple valvues at once like this 
+    console.log(playerName,playerAttack,playerHealth);
+
+    var enemyName = "Roberto";
+    var enemyHealth = 50;
+    var enemyAttack = 12;
+
+    var fight = function(){
+        // alert players they have started a round
+        window.alert("Welcome to robot gladiators1");
+         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+  // Log a resulting message to the console so we know that it worked.
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    enemyHealth = enemyHealth - playerAttack;
+  // Log a resulting message to the console so we know that it worked.
+  console.log (playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining.");
+    };
 
 fight();
